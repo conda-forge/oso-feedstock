a24cefb07de1b5361a82ca5d18c599aa0987bc48
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-06-19 10:21:25 -0500

    revert win skip, add some win retries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,10 +4,13 @@ set OSO_ENV=RELEASE
 
 cargo build --release -p polar-c-api
 
-cd %SRC_DIR%/languages/python/oso
+cd "%SRC_DIR%/languages/python/oso"
 
 make build
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-%PYTHON% -m pip install .
+:: retry a couple times
+"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check ^
+    || "%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check ^
+    || "%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -eux
 
 cargo build --release -p polar-c-api
@@ -7,10 +6,10 @@ cargo build --release -p polar-c-api
 rm -rf target/release
 mv target/*/release target/release
 
-cd $SRC_DIR/languages/python/oso
+cd "${SRC_DIR}/languages/python/oso"
 
 make build OSO_ENV=RELEASE
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-$PYTHON -m pip install .
+"${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check
